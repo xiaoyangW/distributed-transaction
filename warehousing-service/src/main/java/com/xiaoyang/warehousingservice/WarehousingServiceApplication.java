37ac70a351f055aedde74f186239c9dde1d72422
xiaoyang <1483863489@qq.com>
2020-04-06 13:08:36 +0800

    添加seate配置

--- warehousing-service/src/main/java/com/xiaoyang/warehousingservice/WarehousingServiceApplication.java
+++ warehousing-service/src/main/java/com/xiaoyang/warehousingservice/WarehousingServiceApplication.java
@@ -1,9 +1,15 @@
 package com.xiaoyang.warehousingservice;
 
+import com.xiaoyang.warehousingservice.config.DataSourceConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@SpringBootApplication
+@EnableFeignClients
+@EnableDiscoveryClient
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class WarehousingServiceApplication {
 
     public static void main(String[] args) {
